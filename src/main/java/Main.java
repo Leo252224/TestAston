e7a4346c23208eed4378571857ab5347a8c5b326
Leo252224 <leo252224@gmail.com>
2024-02-14 13:15:50 +0330

    новый коммит

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,6 +18,7 @@ public class Main {
             System.out.println("Результат неудался");
         }
         System.out.println("==================================\n");
+        System.out.println();
 
 
         // Задача номер 2
